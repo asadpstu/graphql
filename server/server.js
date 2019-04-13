55575d6dbc70a5832012a9dd182f0f424ed67b89
Asad <hmasad09@gmail.com>
2019-04-13 19:36:43 +0800

    Grapghql setup initial stage

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
-const express = require ('express');
+const express = require('express');
+const GraphQL = require('express-graphql');
 const app = express();
 
+// Creating node Server
 app.listen(4000,()=>{
   console.log("App is listening to 4000 Port");
 });
+
+
+// Creating Graphql End-point
+app.use('/graphql',GraphQL({
+  
+}));
